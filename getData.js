56a2fbc7112160ba9a764ca39abdccfaade2c861
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-08-30 11:57:48 -0400

    Completed

--- getData.js
+++ getData.js
@@ -3,7 +3,17 @@ let response = require('./recipesData.js')
 let data = response.recipesData1
 
 const getData = (data, str) => {
+    let recipes = data.data.recipes;
     
+    let result = recipes.reduce(function(filteredRecipes, recipe){
+        let existsIngredient = recipe.ingredients.find(i=>i.toLowerCase().includes(str));
+        if(existsIngredient != undefined){
+            filteredRecipes.push(recipe.name);
+        }
+        return filteredRecipes;
+        
+    }, []);
+    return result;
 }
 
 console.log(getData(data, 'oil'))
